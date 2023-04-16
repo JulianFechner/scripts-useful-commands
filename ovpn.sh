6c78703d6e259eeb0dc5b93bc10e1c15a6d02f39
JulianFechner <fechnerjulian@gmail.com>
2023-04-16 04:46:40 +0200

    Added script for OVPN MTU-change

--- ovpn.sh
+++ ovpn.sh
@@ -4,15 +4,19 @@
 
 options="Usage: ovpn.sh [OPTIONS]
 	Options:
-	--thm          Connect to THM via OVPN
-	--htb          Connect to HTB via OVPN"
+	--thm			Connect to THM via OVPN
+	--htb-machines 		Connect to HTB machines via OVPN
+	--htb-seasonal 		Connect to HTB seasonal machinesvia OVPN"
 
 if [[ $@ == "--thm" ]]
 then
 	openvpn --config "/home/kali/Documents/vpn/thm/N30n.ovpn"
-elif [[ $@ == "--htb" ]]
+elif [[ $@ == "--htb-machines" ]]
 then
-	openvpn --config "/home/kali/Documents/vpn/htb/xn30n.ovpn"
+	openvpn --config "/home/kali/Documents/vpn/htb/lab_xn30n.ovpn"
+elif [[ $@ == "--htb-seasonal" ]]
+then
+	openvpn --config "/home/kali/Documents/vpn/htb/competitive_xn30n.ovpn"
 else
 	# Removes unnessecary indentation
 	sed 's/^[[:space:]]*//' <<< "$options"
